c12a0005627f34f4c3fa1cedcb206239c70eddff
ivancode <ivanrenescorcia@gmail.com>
2021-01-04 10:27:41 -0500

    middlewares, user routes, ejs view engine added

--- server.js
+++ server.js
@@ -1,11 +1,61 @@
 const express = require('express');
+const morgan = require('morgan')
 const app = express();
 
 
-app.listen(8000, ()=>{
-    console.log('Server on port 8000')
+/* function middleLogger(req, res, next) {
+    console.log(`Route Received: ${req.protocol}://${req.get('host')}${req.originalUrl}`)
+    next();
+}
+ */
+
+// SETTINGS
+app.set('appName', 'ExpressJSCourse' )
+app.set('port', 5000)
+app.set('view engine', 'ejs')
+const port = app.get('port');
+
+
+
+ //MIDDLEWARES
+app.use(express.json());
+app.use(morgan('dev'));
+
+//ROUTES
+app.get('/user' , (req, res) =>{
+    res.json({
+        username: 'ivancode',
+        email: 'ivanrenescorcia@gmail.com'
+    })
 })
 
-app.get('/' , (req, res) =>{
-    res.send('Hello user');
+app.post('/user/:id' , (req, res) =>{
+    console.log(req.body)
+    console.log(req.params)
+    res.json(req.body)
 })
+
+app.get('/user/:id' , (req, res) =>{
+    res.json({
+        username: 'ivancode',
+        email: 'ivanrenescorcia@gmail.com'
+    })
+})
+
+
+
+app.get('/test' , (req, res) =>{
+    const data = [{fullname:'Ivan Rodriguez'},{fullname:'Hecto Cohen'},{fullname:'Sebastian Sandoval'}, {fullname : 'Juan Andres'}]
+    res.render('index.ejs' ,{people : data})
+})
+
+
+app.use(express.static('public'))
+
+app.listen(port, ()=>{
+    console.log(app.get('appName'))
+    console.log('Server on port ', port)
+})
+
+//TODO
+//CREATE AN APP WITH EXPRESSJS, NODEJS,d MONGODB AND REACTJS
